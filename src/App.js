016cb1e03073cb5eb6ba08e393e7804a57a40c3b
Grover Richardson <groverrichardson1987@gmail.com>
2020-10-26 19:17:36 -0700

    Initial commit, hero section created and styled.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import EmergencyButton from './components/emergency-button';
+import Menu from './components/menu';
+import RequestButton from './components/request-button';
+import 'normalize.css';
+import './sass/style.sass';
+import heroImg from './assets/hero-image/hero-image.png';
+import tealCircle from './assets/teal-circle.svg';
+import pinkCircle from './assets/pink-circle.svg';
+import yellowCircle from './assets/yellow-circle.svg';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <main className="App">
+            <Menu />
+            <section className="hero-section">
+                <div className="hero-copy-container">
+                    <h1 className="hero-header">
+                        Your Best Smile Is One Visit Away
+                    </h1>
+                    <p className="hero-copy">
+                        Donec id elit non mi porta gravida at eget metus. Cum
+                        sociis natoque penatibus et magnis dis parturient
+                        montes, nascetur ridiculus mus.
+                    </p>
+                    <div className="hero-button-container">
+                        <RequestButton />
+                        <EmergencyButton />
+                    </div>
+                </div>
+                <div className="image-container">
+                    <img
+                        src={heroImg}
+                        alt="Dental patients smiling"
+                        className="hero-image"
+                    />
+                    <img
+                        src={tealCircle}
+                        alt="Decorative circle"
+                        className="teal-circle-hero circle"
+                    />
+                    <img
+                        src={yellowCircle}
+                        alt="Decorative circle"
+                        className="yellow-circle-hero circle"
+                    />
+                    <img
+                        src={pinkCircle}
+                        alt="Decorative circle"
+                        className="pink-circle-hero circle"
+                    />
+                </div>
+            </section>
+        </main>
+    );
 }
 
 export default App;
---
+++ src/components/request-button.js
@@ -0,0 +1,10 @@
+import React from 'react';
+// import { Link } from 'react-dom';
+
+export default class RequestButton extends React.Component {
+    render() {
+        return (
+            <button className="request-button">Request an Appointment</button>
+        );
+    }
+}
